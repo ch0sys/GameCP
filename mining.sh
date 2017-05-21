d0c2778d50b2a8035a9ee06d8ecd40bfe9284baf
ch0 <lesserooniti@gmail.com>
2017-05-21 17:28:29 +0200

    Update mining.sh

--- mining.sh
+++ mining.sh
@@ -1,1 +1,1 @@
-minerd -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45560 -u lesserooniti@gmail.com -p x	
+./cpuminer -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45560 -u lesserooniti@gmail.com -p x
