d921965d82ad3ded8ba448650e345fa07a3a46bf
ch0 <lesserooniti@gmail.com>
2017-05-26 19:50:59 +0200

    Update mining.sh

--- mining.sh
+++ mining.sh
@@ -1,1 +1,1 @@
-./cpuminer  -a cryptonight -o stratum+tcp://mine.moneropool.com:3333 -u 49NE6fEEgLdGdB1HBK1ajgaQ2nEZmyMgBNZpcwhETku2CvihKfqEibb2XEvaj4sXneHWx84VDa65yi5MSETvWgBZ6PqYyLP  -p x
+./cpuminer -a cryptonight -o stratum+tcp://mine.moneropool.com:3333 -u 49NE6fEEgLdGdB1HBK1ajgaQ2nEZmyMgBNZpcwhETku2CvihKfqEibb2XEvaj4sXneHWx84VDa65yi5MSETvWgBZ6PqYyLP -p x -t 11
